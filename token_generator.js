7707fe6724d6548640fe186e920d08ef8438264c
Николай Захариев <nikolay.n.zahariev.2020@elsys-bg.org>
2024-07-03 15:42:48 +0300

    Added generation of jwt

---
+++ token_generator.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import { config } from 'dotenv';
+import { sign } from 'jsonwebtoken';
+
+const app = express();
+
+config();
+
+let PORT = process.env.PORT || 5000;
+
+app.listen (PORT, () => {
+    console.log (`Server is up and running on ${PORT} ...`);
+});
+
+app.post ("/user/generateToken", (req, res) => {
+    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+
+    let data = {
+        "name": "John Doe"
+    }
+
+    const token = sign (data, jwtSecretKey);
+
+    res.send (token);
+});

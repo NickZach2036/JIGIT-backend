d2a6ce3281ec92db8c134ea093caeacf4ea20894
Николай Захариев <nikolay.n.zahariev.2020@elsys-bg.org>
2024-07-05 08:09:06 +0300

    Added utils folder and changed files

---
+++ src/utils/sequelize_setup.js
@@ -0,0 +1,13 @@
+import { Sequelize } from 'sequelize';
+import 'dotenv/config';
+
+const sequelize = new Sequelize({
+  database: process.env.POSTGRES_DB,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.DATABASE_PORT,
+  dialect: 'postgres',
+});
+
+export default sequelize;
